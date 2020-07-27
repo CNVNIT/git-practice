4b4aec0250dcb324dcb72e8084eee104ddd17af7
CNVNIT <56714918+CNVNIT@users.noreply.github.com>
2020-07-27 12:20:36 -0700

    rename heroku

--- miniapp.js
+++ miniapp.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("1-2-3-4-5-6-7-8-9-0");
 });
 
 let a = [];
